4409b1dcc04cebcd43d705d6cd8373a7b436aea4
Rono <victorrono18@gmail.com>
2021-08-18 17:52:28 +0300

    travelApp

---
+++ server/routes/users.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user._id);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password,
+      });
+      !user && res.status(400).json("Wrong username or password");
+
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      !validPassword && res.status(400).json("Wrong username or password");
+
+      res.status(200).json({ _id: user._id, username: username });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+});
+
+module.exports = router;
